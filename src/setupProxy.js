c2ad27e882b2d67a30012ed0007b70e9812002db
Karthik <karthik.venkatesan@softsuave.com>
2024-05-31 02:31:52 +0530

    url changes in proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: "http://13.201.247.48/api/",
+            target: "http://13.201.247.48",
             changeOrigin: true,
         })
     );
