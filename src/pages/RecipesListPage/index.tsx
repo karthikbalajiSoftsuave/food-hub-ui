7938511c5d945629b69b1bcf111b554c40ce7851
godwindavid <godwin.softsuave@gmail.com>
2024-05-31 13:55:03 +0530

    category change

--- src/pages/RecipesListPage/index.tsx
+++ src/pages/RecipesListPage/index.tsx
@@ -28,7 +28,7 @@ const filterProps = [
       { label: "Average Rating", value: "avg_rating" },
       { label: "Preparation Steps", value: "preparation_steps" },
       { label: "Serving Size", value: "serving_size" },
-      { label: "Category Id", value: "category_id" },
+      { label: "Category", value: "category_id" },
       { label: "Ingredients", value: "ingredients" },
     ],
   },
